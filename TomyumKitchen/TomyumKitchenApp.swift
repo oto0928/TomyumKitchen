22efd50fb68a84aec3150f52a04ad79b72ce1b56
toa takeuchi <toa0928koron@icloud.com>
2025-09-27 01:18:29 +0900

    Initial Commit

---
+++ TomyumKitchen/TomyumKitchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomyumKitchenApp.swift
+//  TomyumKitchen
+//
+//  Created by 竹内音碧 on 2025/09/27.
+//
+
+import SwiftUI
+
+@main
+struct TomyumKitchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
